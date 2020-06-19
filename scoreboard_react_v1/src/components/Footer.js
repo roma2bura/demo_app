82b260e7a47dbe25dfe15cddec85b99b3ffc8092
Roman Bura <r2bcloud@gmail.com>
2020-06-19 16:38:44 -0700

    friday update

---
+++ scoreboard_react_v1/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="devfooter">
+        <small>[ Dev Roman Bura ]</small>
+      </div>
+    );
+  }
+}
+
+export default Footer;
